1714fffbbbb6950be36c9993eeaa7fb21ce97f40
doobo <doobo@foxmail.com>
2021-06-21 01:23:22 +0800

    更新打包脚本

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-git init .
-git remote add origin git@github.com:doobo/union-cache.git
-git add .
-git commit -m "版本更新"
-git push origin master --force
-
-git remote add gitee git@gitee.com:doobo/union-cache.git
-git push gitee master --force
+#git init .
+#git remote add origin git@github.com:doobo/union-cache.git
+#git add .
+#git commit -m "版本更新"
+#git push origin master --force
+#
+#git remote add gitee git@gitee.com:doobo/union-cache.git
+#git push gitee master --force
 
 mvn clean
 mvn release:prepare
